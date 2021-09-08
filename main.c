ea921696678c2d880ea2ce53a2f1e5f7745020ae
aperinus <s193939@student.dtu.dk>
2021-09-08 16:58:57 +0200

    toGreyScale

--- main.c
+++ main.c
@@ -2,22 +2,29 @@
 //To run (linux/mac): ./main.out example.bmp example_inv.bmp
 
 //To compile (win): gcc cbmp.c main.c -o main.exe -std=c99
-//To run (win): main.exe example.bmp example_inv.bmp
+//To run (win): ./main.exe example.bmp example_inv.bmp
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
 
 //Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void toGreyScale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+  int sum;
+
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
+      sum=sum+input_image[x][y][c];
       }
+      output_image[x][y][0] = sum/3;
+      output_image[x][y][1] = sum/3;
+      output_image[x][y][2] = sum/3;
+      
+      sum = 0;
     }
   }
 }
@@ -46,7 +53,8 @@ int main(int argc, char** argv)
   //Load image from file
   read_bitmap(argv[1], input_image);
 
-  
+  //To grey
+  toGreyScale(input_image,output_image);
 
   //Save image to file
   write_bitmap(output_image, argv[2]);
