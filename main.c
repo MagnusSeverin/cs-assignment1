625b40e069dd928ba30b805c4c02f63a55b16445
SimonBuk98 <Simonbuk98@gmail.com>
2021-09-15 11:33:32 +0200

    Update main.c
    Binary Threshold, er blevet added.

--- main.c
+++ main.c
@@ -9,32 +9,56 @@
 #include "cbmp.h"
 
 //Function to invert pixels of an image (negative)
-void toGreyScale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+void toGreyScale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
   int sum;
-
   for (int x = 0; x < BMP_WIDTH; x++)
   {
     for (int y = 0; y < BMP_HEIGTH; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      sum=sum+input_image[x][y][c];
+        sum += input_image[x][y][c];
       }
-      output_image[x][y][0] = sum/3;
-      output_image[x][y][1] = sum/3;
-      output_image[x][y][2] = sum/3;
-      
+      output_image[x][y][0] = sum / 3;
+      output_image[x][y][1] = sum / 3;
+      output_image[x][y][2] = sum / 3;
       sum = 0;
     }
   }
 }
+void binaryThreshold(unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
+  int Th = 90;
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      for (int c = 0; c < BMP_CHANNELS; c++)
+      {
+        if (output_image[x][y][c] <= Th)
+        {
+          output_image[x][y][0] = 0;
+          output_image[x][y][1] = 0;
+          output_image[x][y][2] = 0;
+        }
+        else
+        {
+          output_image[x][y][0] = 255;
+          output_image[x][y][1] = 255;
+          output_image[x][y][2] = 255;
+        }
+      }
+    }
+  }
+}
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+//Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
 //Main function
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   //argc counts how may arguments are passed
   //argv[0] is a string with the name of the program
@@ -44,8 +68,8 @@ int main(int argc, char** argv)
   //Checking that 2 arguments are passed
   if (argc != 3)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
   }
 
   printf("Example program - 02132 - A1\n");
@@ -54,7 +78,10 @@ int main(int argc, char** argv)
   read_bitmap(argv[1], input_image);
 
   //To grey
-  toGreyScale(input_image,output_image);
+  toGreyScale(input_image, output_image);
+
+  //to two colors
+  binaryThreshold(output_image);
 
   //Save image to file
   write_bitmap(output_image, argv[2]);
